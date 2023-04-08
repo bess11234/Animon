ce466c13afb2b9c9ef91d026b899fc4a122b0419
Cho <112687367+Cninathip@users.noreply.github.com>
2023-04-08 18:48:05 +0700

    Add files via upload
    update name and animon can dead

--- Animon.java
+++ Animon.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public abstract class Animon implements Attackable{
+    protected String name;
     protected int level;
     protected int hp;
     protected int stamina = level*5;
@@ -11,7 +12,13 @@ public abstract class Animon implements Attackable{
         attacker.stamina += attacker.baseAtk;
     }
     public void attacked(Animon attacker, int damage){
-        this.hp -= damage;
+        if(this.hp - damage < 0){
+            System.out.println("Your animon die.");
+            this.dead();
+        }
+        else{
+            this.hp -= damage;
+        }
     }
     public void nomalAttack(Animon atked){
         atked.attacked(this, this.baseAtk);
@@ -22,10 +29,14 @@ public abstract class Animon implements Attackable{
     public void useskill(Animon atked, String skillName){
         int multi = (int)skill.get(skillName);
         if(this.stamina > this.level*multi){
+            this.stamina -= level*multi;
             atked.attacked(this, multi*this.baseAtk);
         }
         else{
             System.out.println("Your stamina are not enough.");
         }
+    }
+    public void dead(){
+        //wait for player
     }
 }

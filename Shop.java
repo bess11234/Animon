02df5c4963a343ccf31d3bf6206ac15c1880927d
65070014 <112687540+65070014@users.noreply.github.com>
2023-04-10 16:14:59 +0700

    Add files via upload

--- Shop.java
+++ Shop.java
@@ -1,28 +1,23 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Shop {
-    public static void itemShop() throws InterruptedException{
+    private Item[] itemShop = new Item[5];
+    
+    public void sellItem(Player player, int num){
+        player.getInventory().add(itemShop[num]);
+    }
+    public Shop(){
         Scanner buy = new Scanner(System.in);
-        ArrayList<String> itemShop = new ArrayList();
-        itemShop.add("1.Potion Hp 70%");
-        itemShop.add("2.Potion Hp 30%");
-        itemShop.add("3.Potion Stamina 70%");
-        itemShop.add("4.Potion Stamina 30%");
-        itemShop.add("5.AniBall");
+        itemShop[0] = new PotionHpSmall();
+        itemShop[1] = new PotionHpBig();
+        itemShop[2] = new PotionStaminaSmall();
+        itemShop[3] = new PotionStaminaBig();
+        itemShop[4] = new AniBall();
         
-        for(int i = 0; i < itemShop.size(); i++){
-            System.out.println(itemShop.get(i));
+        for (Item itemShop1 : itemShop) {
+            System.out.println(itemShop1);
         }
         
-        System.out.println("\nWhat items you wanna buy ? 1-5");
-        String buyItems = buy.nextLine();
-        itemShop.remove(buyItems);
-        Thread.sleep(1000);
-        System.out.println("ok wait a sec...");
-        Thread.sleep(7000);
-        System.out.println("You just got scamed xDDDD");
     }
     
 }
